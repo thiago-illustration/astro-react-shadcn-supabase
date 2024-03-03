74659d3d7618a47adc92c78ad0078c9bec8986d8
thiago-illustration <thiago.ilustracao@gmail.com>
2024-03-03 10:18:52 -0300

    fix(chore): fixed import aliases and added middlewares lib

---
+++ src/lib/middlewares/auth/index.ts
@@ -0,0 +1,57 @@
+import { defineMiddleware } from "astro:middleware";
+
+import { supabase } from "@lib/supabase";
+import { PUBLIC_ONLY_PAGES } from "@lib/constants/public-only-pages";
+import { PRIVATE_PAGES } from "@lib/constants/private-pages";
+
+export const withAuth = defineMiddleware(
+  async ({ locals, url, cookies, redirect }, next) => {
+    if (PRIVATE_PAGES.includes(url.pathname)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (!accessToken || !refreshToken) {
+        return redirect("/signin");
+      }
+
+      const { data, error } = await supabase.auth.setSession({
+        refresh_token: refreshToken.value,
+        access_token: accessToken.value,
+      });
+
+      if (error) {
+        cookies.delete("sb-access-token", {
+          path: "/",
+        });
+        cookies.delete("sb-refresh-token", {
+          path: "/",
+        });
+        return redirect("/signin");
+      }
+
+      locals.user = data.user!;
+      locals.session = data.session!;
+
+      cookies.set("sb-access-token", data?.session?.access_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
+      cookies.set("sb-refresh-token", data?.session?.refresh_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
+    }
+
+    if (PUBLIC_ONLY_PAGES.includes(url.pathname)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (accessToken && refreshToken) {
+        return redirect("/dashboard");
+      }
+    }
+    return next();
+  },
+);
